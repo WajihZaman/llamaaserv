bde81fcaed1c1a76c8cef050d8a6f6bad3735e31
Wajih Uz Zaman <wajihzaman99@gmail.com>
2026-05-31 09:10:57 +0400

    Update schema.py

--- central/schema.py
+++ central/schema.py
@@ -6,5 +6,5 @@ from uuid import UUID
 class UserQuery(BaseModel):
     uid: UUID | None = UUID("d4123af6-f0c4-4707-a5b1-9428839b42f3")
     emp_no: int | None = 12345678 # Employee ID field
-    convo_id: str | None = "conversation code"
-    msg: str | None = "Introduce yourself"
+    convo_id: str | None = "conversation code" # Conversation ID
+    msg: str | None = "Introduce yourself" # User query
